8c337aad4d38a3fbf31dadc1cdf044e73ac23da2
LiekeGeudens <157026470+LiekeGeudens@users.noreply.github.com>
2024-03-17 12:44:53 +0100

    Update download_datasets.R

--- src/data-preparation/download_datasets.R
+++ src/data-preparation/download_datasets.R
@@ -10,9 +10,6 @@ list_urls <- list(c(url='http://data.insideairbnb.com/the-netherlands/north-holl
                   c(url='http://data.insideairbnb.com/japan/kant%C5%8D/tokyo/2023-12-27/data/listings.csv.gz', fn='tyo_listings.csv.gz')
 )
 
-#names for earch dataset
-names <- c('Amsterdam_listings', 'London_listings', 'Tokyo_listings')
-
 for (i in 1:length(urls)) {
   download.file(lists_urls[i], paste0("../../data/", names[i], ".csv.gz"), mode = "wb")
 }
