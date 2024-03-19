049f4e98418e5f2e51f9b244eb06314fd5ec4ac7
LiekeGeudens <157026470+LiekeGeudens@users.noreply.github.com>
2024-03-19 17:53:44 +0100

    Create Install_packages.R

---
+++ src/data-preparation/Install_packages.R
@@ -0,0 +1,8 @@
+# install all required packages
+install.packages("broom")
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("tidyverse")
+install.packages("readr")
+install.packages("data.table")
+install.packages("DescTools")
