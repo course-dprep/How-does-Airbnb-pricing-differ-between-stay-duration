dca40524e027dbed5e5e1638668f025e001beff6
Lingwei Zhao <157130986+LZhao97@users.noreply.github.com>
2024-03-23 13:53:49 +0100

    Delete src/data-preparation/Install_packages.R

--- src/data-preparation/Install_packages.R
+++
@@ -1,9 +0,0 @@
-# install all required packages
-install.packages("broom")
-install.packages("dplyr")
-install.packages("ggplot2")
-install.packages("tidyverse")
-install.packages("readr")
-install.packages("data.table")
-install.packages("DescTools")
-
